8200fcee4110cd8338a8281c191c807b004680c8
Ahmed Al-Jafarawy <prog.al.jafarawy@gmail.com>
2025-04-01 05:34:06 +0200

    link remote repo

---
+++ data/cart.js
@@ -0,0 +1,63 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {
+      productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+      quantity: 2,
+      deliveryOptionId: '2',
+    },
+    {
+      productId: '8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+      quantity: 4,
+      deliveryOptionId: '3',
+    },
+  ];
+}
+cart.forEach((e) => {
+  console.log(e.deliveryOptionId);
+});
+function addToLocalStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addTocart(productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '3',
+    });
+  }
+  addToLocalStorage();
+}
+
+export function removeItemFromCart(productIdWilDelete) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productIdWilDelete) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  addToLocalStorage();
+}
+
+export function updateDeliveryOptionDate(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  addToLocalStorage();
+}
